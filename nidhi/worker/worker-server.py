4211fb2796c46a93e3160ac2008c7df13340d5c7
nidhichoudhary1411 <nidz456.dhn@gmail.com>
2024-12-07 01:52:26 -0700

    added worker code

--- nidhi/worker/worker-server.py
+++ nidhi/worker/worker-server.py
@@ -9,6 +9,7 @@ import requests
 import hashlib
 import time
 import bs4
+import transformers
 from bs4 import BeautifulSoup
 import pandas as pd
 from transformers import pipeline
@@ -22,46 +23,57 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import nltk
 from collections import Counter
 
-
 # Initialize logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Initialize Redis connection
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
 
-# Configure MinIO client using environment variables
+# Configure MinIO client
 minio_client = Minio(
-    'minio-proj.minio-ns.svc.cluster.local:9000',  # MinIO server host and port
-    access_key='rootuser',  # MinIO root username
-    secret_key='rootpass123',  # MinIO root password
-    secure=False  # Disable SSL (assuming MinIO is not using HTTPS)
+    'minio-proj.minio-ns.svc.cluster.local:9000',
+    access_key='rootuser',
+    secret_key='rootpass123',
+    secure=False
 )
 
 # Ensure that the bucket exists
 bucket_name = "reviews"
-output_bucket_name = "analysis_output"
-if not minio_client.bucket_exists(bucket_name):
-    minio_client.make_bucket(bucket_name)
-    logging.debug(f"Bucket '{bucket_name}' created in MinIO.")
+try:
+    if not minio_client.bucket_exists(bucket_name):
+        minio_client.make_bucket(bucket_name)
+        logging.info(f"Bucket '{bucket_name}' created in MinIO.")
+except S3Error as e:
+    logging.error(f"Error with MinIO: {e}")
 
-if not minio_client.bucket_exists(output_bucket_name):
-    minio_client.make_bucket(output_bucket_name)
-    logging.debug(f"Bucket '{output_bucket_name}' created in MinIO.")
+output_bucket_name = "analysisoutput"
+try:
+    if not minio_client.bucket_exists(output_bucket_name):
+        minio_client.make_bucket(output_bucket_name)
+        logging.info(f"Bucket '{output_bucket_name}' created in MinIO.")
+except S3Error as e:
+    logging.error(f"Error with MinIO: {e}")
 
-# Amazon scraping functions
+# Amazon scraping constants
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
     'Accept-Language': 'en-US, en;q=0.5'
 }
 
 def getdata(url):
-    r = requests.get(url, headers=HEADERS)
-    return r.text
+    try:
+        r = requests.get(url, headers=HEADERS, timeout=10)
+        r.raise_for_status()
+        return r.text
+    except requests.RequestException as e:
+        logging.error(f"Error fetching URL {url}: {e}")
+        return None
 
 def html_code(url):
     htmldata = getdata(url)
-    soup = BeautifulSoup(htmldata, 'html.parser')
-    return soup
+    if htmldata:
+        return BeautifulSoup(htmldata, 'html.parser')
+    return None
 
 def get_product_info(soup):
     product_name = soup.find('span', {'id': 'productTitle'})
@@ -87,11 +99,16 @@ def get_reviews(soup):
 def get_reviews_from_multiple_pages(product_url, num_pages=5):
     reviews = []
     soup = html_code(product_url)
+    if not soup:
+        return reviews
+
     product_name, user_rating = get_product_info(soup)
     
     for page in range(1, num_pages + 1):
         paginated_url = f"{product_url}?pageNumber={page}"
         soup = html_code(paginated_url)
+        if not soup:
+            continue
         page_reviews = get_reviews(soup)
         
         for review in page_reviews:
@@ -137,32 +154,70 @@ def improved_summary(reviews):
 
     return summary
 
+# Function to classify emotions
+def classify_emotions(reviews):
+    model_name = "joeddav/distilbert-base-uncased-go-emotions-student"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    positive_emotions = {"admiration", "amusement", "approval", "caring", "excitement", 
+                         "gratitude", "joy", "love", "optimism", "pride", "relief", "surprise"}
+    negative_emotions = {"anger", "annoyance", "disappointment", "disapproval", 
+                         "disgust", "embarrassment", "fear", "grief", "remorse", "sadness"}
+    neutral_emotions = {"confusion", "curiosity", "desire", "nervousness", "realization"}
+
+    positive_score = 0
+    negative_score = 0
+    neutral_score = 0
+
+    for review in reviews:
+        inputs = tokenizer(review, return_tensors="pt", truncation=True, max_length=512)
+        outputs = model(**inputs)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=-1).detach().numpy()
+
+        emotion_labels = [
+            "admiration", "amusement", "anger", "annoyance", "approval", "caring", "confusion",
+            "curiosity", "desire", "disappointment", "disapproval", "disgust", "embarrassment",
+            "excitement", "fear", "gratitude", "grief", "joy", "love", "nervousness", "optimism",
+            "pride", "realization", "relief", "remorse", "sadness", "surprise"
+        ]
+
+        for i, emotion in enumerate(emotion_labels):
+            if emotion in positive_emotions:
+                positive_score += probabilities[0][i]
+            elif emotion in negative_emotions:
+                negative_score += probabilities[0][i]
+            elif emotion in neutral_emotions:
+                neutral_score += probabilities[0][i]
+
+    total_score = positive_score + negative_score + neutral_score
+    positive_percentage = (positive_score / total_score) * 100
+    negative_percentage = (negative_score / total_score) * 100
+    neutral_percentage = (neutral_score / total_score) * 100
+
+    return positive_percentage, negative_percentage, neutral_percentage
+
 def process_task(reviewhash):
     try:
-        # Retrieve Amazon URL from Redis
-        amazon_url = redis_client.hget(reviewhash, "amazon_url")
-        if not amazon_url:
-            logging.error(f"No Amazon URL provided for reviewhash {reviewhash}")
-            return
+        logging.info(f"Starting task for reviewhash: {reviewhash}")
+        redis_client.hset(reviewhash, "status", "processing")
 
-        # Scrape reviews
+        amazon_url = redis_client.hget(reviewhash, "link")
+        if not amazon_url:
+            raise ValueError(f"No Amazon URL provided for reviewhash {reviewhash}")
+
+        logging.info(f"Scraping reviews for URL: {amazon_url}")
         reviews_data = get_reviews_from_multiple_pages(amazon_url)
         if not reviews_data:
-            logging.error(f"No reviews found for {amazon_url}")
-            return
+            raise ValueError(f"No reviews found for {amazon_url}")
 
-        # Save reviews to CSV
         csv_file = f"/tmp/{reviewhash}_reviews.csv"
         df = pd.DataFrame(reviews_data)
         df.to_csv(csv_file, index=False)
-        logging.debug(f"CSV file created: {csv_file}")
+        logging.info(f"CSV file created: {csv_file}")
 
-        # Upload CSV to MinIO
-        csv_object_path = f"{reviewhash}/reviews.csv"
-        minio_client.fput_object(bucket_name, csv_object_path, csv_file)
-        logging.debug(f"Uploaded CSV to MinIO: {csv_object_path}")
-
-        # ANALYSIS: Generate the summary of the reviews
+         # ANALYSIS: Generate the summary of the reviews
         summary = improved_summary(df['Review'])
 
         # Save summary to DataFrame and then upload it to another MinIO bucket
@@ -176,15 +231,64 @@ def process_task(reviewhash):
         minio_client.fput_object(output_bucket_name, summary_object_path, summary_file)
         logging.info(f"Uploaded summary to MinIO: {summary_object_path}")
 
+        # Sentiment analysis
+        logging.info(f"Classifying emotions for reviews in task: {reviewhash}")
+        reviews = df['Review'].tolist()
+        positive, negative, neutral = classify_emotions(reviews)
+
+        # Save sentiment analysis results
+        sentiment_df = pd.DataFrame({
+            'positive': [positive],
+            'neutral': [neutral],
+            'negative': [negative]
+        })
+        sentiment_file = f"/tmp/{reviewhash}_sentiment.csv"
+        sentiment_df.to_csv(sentiment_file, index=False)
+        
+        sentiment_object_path = f"{reviewhash}/sentiment_analysis.csv"
+        minio_client.fput_object(output_bucket_name, sentiment_object_path, sentiment_file)
+        logging.info(f"Sentiment analysis saved to MinIO: {sentiment_object_path}")
+
+        # Extract product name and update the callback in Redis
+        product_names = df['Product Name'].drop_duplicates().tolist()
+        if product_names:
+            product_name = product_names[0]  # Assuming the first name is the desired one
+            logging.info(f"Extracted product name: {product_name}")
+
+            # Set the callback value in Redis
+            redis_client.hset(reviewhash, "callback", product_name)
+            logging.info(f"Set callback for {reviewhash}: {product_name}")
+
+        # Update the status to completed
+        redis_client.hset(reviewhash, "status", "completed")
+        logging.info(f"Task completed for reviewhash: {reviewhash}")
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Network error for {reviewhash}: {str(e)}")
+        redis_client.hset(reviewhash, "status", "failed")
+    except ValueError as e:
+        logging.error(str(e))
+        redis_client.hset(reviewhash, "status", "failed")
     except Exception as e:
-        logging.error(f"Error processing task for {reviewhash}: {str(e)}")
+        logging.error(f"Unexpected error for {reviewhash}: {str(e)}")
+        redis_client.hset(reviewhash, "status", "failed")
+
 
 def worker():
-    logging.debug("Worker is listening for tasks in the Redis queue...")
+    logging.info("Worker is listening for tasks in the Redis queue...")
     while True:
-        reviewhash = redis_client.brpop("toWorker", 0)[1]
-        logging.debug(f"Received task for review with hash: {reviewhash}")
-        process_task(reviewhash)
+        try:
+            task = redis_client.brpop("linkQueue", 0)
+            if task:
+                reviewhash = task[1]
+                logging.info(f"Received task for review with hash: {reviewhash}")
+                process_task(reviewhash)
+        except redis.RedisError as e:
+            logging.error(f"Redis error: {e}")
+            time.sleep(5)  # Wait before retrying
+        except Exception as e:
+            logging.error(f"Unexpected error in worker loop: {e}")
+            time.sleep(5)  # Wait before retrying
 
 if __name__ == "__main__":
     worker()
